2cbba2070386b32d2042986fb2d998bc7358b8f2
Gopal Kumar <rahulmishra929292@GMAIL.COM>
2026-05-19 20:34:21 +0530

    temporary open cors

--- server/index.js
+++ server/index.js
@@ -11,11 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://wavetune-two.vercel.app',
-    'https://wavetune-git-main-gopaljjha-s-projects4.vercel.app'
-  ],
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
